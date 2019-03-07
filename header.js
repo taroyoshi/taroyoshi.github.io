401f0f364537871a4d107ab1c8d746874e2c75dd
TaroYoshi <jetworldcircus@gmail.com>
2019-03-07 22:18:54 +0900

    jitaku chrome

--- header.js
+++ header.js
@@ -1,5 +1,6 @@
 $(function(){
-    $("#header-outer").load("../html/header.html #header-inner");
+    //$("#header-outer").load("../html/header.html #header-inner");
+    $("#header-outer").load("../html/header.html");
 });
 
 //TODO 表示ページのリンク削除等
@@ -12,8 +13,8 @@ function switch_page(num){
         case 0:
             
             //document.getElementById("index_button").remove();
-            button_elements = document.getElementById("index_button");
-            button_elements.remove();
+            //button_elements = document.getElementById("index_button");
+            //button_elements.remove();
             break;
             
         case 1:
