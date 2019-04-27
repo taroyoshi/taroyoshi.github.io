d191476088a6e906e4499e23e70264fcae4fdbdf
taroyoshi <jetworldcircus@gmail.com>
2019-04-27 04:40:24 +0000

    preview 調査 slick 実装予定

--- preview.js
+++ preview.js
@@ -1,4 +1,9 @@
-function preview(){
+function preview(num){
+    //https://ferret-plus.com/9396
+    //https://webdesignday.jp/inspiration/technique/jquery-js/3847/
+    modalResize("#preview");
     
-    modalResize
+    imgurl = "/img/pre" + num;
+    $("#preview").css("background-image", imgurl);
+    $("#preview").fadeIn("slow");
 }
