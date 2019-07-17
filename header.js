274e4902e73924256b76813c0d641dc5efa3375d
taroyoshi <jetworldcircus@gmail.com>
2019-07-17 03:01:28 +0000

    0717 DC

--- header.js
+++ header.js
@@ -6,6 +6,7 @@ $(function(){
     setTimeout(function(){
         
         //以後 ↓不要の予定?
+        /*
         var gatFile = document.location.pathname.split("/").pop();
         var id;
         switch(gatFile.replace(".html", "")){
@@ -24,10 +25,27 @@ $(function(){
         button_elements = document.getElementById(id);
         $("#" + id).parent().css('background-color', '#d67877');
         button_elements.remove();
+        */
         //以後 ↑不要の予定?
     
         setHover();
         
+        //キーバリューとか使ってスマートに
+        
+        var indexele    = document.getElementById('index').getBoundingClientRect();
+        var profele     = document.getElementById('profile').getBoundingClientRect();
+        var bioele     = document.getElementById('biography').getBoundingClientRect();
+        var skillele     = document.getElementById('skill').getBoundingClientRect();
+        var productele  = document.getElementById('product').getBoundingClientRect();
+        var iidaxeele   = document.getElementById('iidaxe_wrap').getBoundingClientRect();
+        
+        topY = indexele.top + window.pageYOffset;   // Y座標(絶対座標)
+        pflY = profele.top + window.pageYOffset;   // Y座標(絶対座標)
+        bioY = bioele.top + window.pageYOffset;   // Y座標(絶対座標)
+        skillY = skillele.top + window.pageYOffset;   // Y座標(絶対座標)
+        pdtY = productele.top + window.pageYOffset;   // Y座標(絶対座標)
+        idxY = iidaxeele.top + window.pageYOffset;   // Y座標(絶対座標)
+            
         
         //スムーススクロール
         $('a[href^="#"]').click(function(){
@@ -35,29 +53,46 @@ $(function(){
             var speed = 500;
             var href= $(this).attr("href");
             var target = $(href == "#" || href === "" ? 'html' : href);
-            var position = target.offset().top;
+            var position = target.offset().top - 69;
             $("html, body").animate({scrollTop:position}, speed, "swing");
             
-            var indexele    = document.getElementById('index').getBoundingClientRect();
-            var nameele     = document.getElementById('name').getBoundingClientRect();
-            var productele  = document.getElementById('product').getBoundingClientRect();
-            var iidaxeele   = document.getElementById('iidaxe_wrap').getBoundingClientRect();
-            topY = indexele.top + window.pageYOffset;   // Y座標(絶対座標)
-            pflY = nameele.top + window.pageYOffset;   // Y座標(絶対座標)
-            pdtY = productele.top + window.pageYOffset;   // Y座標(絶対座標)
-            idxY = iidaxeele.top + window.pageYOffset;   // Y座標(絶対座標)
-            
-            
-            
-        
             return false;
         
         });
         
         window.addEventListener('scroll', function() {
                            
-            nowYpos = window.pageYOffset;
+            nowYpos = window.pageYOffset + 70;
             
+            var cArray = [3];
+            var sArray = [3];
+            
+            if((nowYpos >= topY) && (pflY > nowYpos)){
+                cArray = [1, 0, 0];
+                sArray = [0, 0, 0];
+            }
+            else if((nowYpos >= pflY) && (bioY > nowYpos)){
+                cArray = [0, 1, 0];
+                sArray = [0, 0, 0];
+            }
+            else if((nowYpos >= bioY) && (skillY > nowYpos)){
+                cArray = [0, 1, 0];
+                sArray = [1, 0, 0];
+            }
+            else if((nowYpos >= skillY) && (pdtY > nowYpos)){
+                cArray = [0, 1, 0];
+                sArray = [0, 1, 0];
+            }
+            else if((nowYpos >= pdtY) && (idxY > nowYpos)){
+                cArray = [0, 0, 1];
+                sArray = [0, 0, 0];
+            }
+            else if(nowYpos >= idxY){
+                cArray = [0, 0, 1];
+                sArray = [0, 0, 1];
+            }
+            
+            changeColor(cArray, sArray);
             
         }, false);
         
@@ -65,7 +100,37 @@ $(function(){
     
 });
 
-// 元色background-color: rgb(255,175,255);
+
+
+function changeColor(arr1, arr2){
+    
+    var carr = ['top_button', 
+                'prof_button', 
+                'prod_button'];
+                
+    var sarr = ['bio_button', 
+                'skill_button', 
+                'iidaxe_button'];
+    
+    for(var i = 0; i < carr.length; i++){
+        if(arr1[i] == 1){
+            $("#" + carr[i]).css('background-color', '#d67877'); 
+        }
+        else if(arr1[i] === 0){
+            
+            $("#" + carr[i]).css('background-color', 'rgb(255,175,255)'); 
+        }
+    }
+    for(var j = 0; j < sarr.length; j++){
+        if(arr2[j] == 1){
+            $("#" + sarr[j]).css('background-color', 'rgb(100,175,155)'); 
+        }
+        else if(arr2[j] === 0){
+            
+            $("#" + sarr[j]).css('background-color', 'rgb(200,175,255)'); 
+        }
+    }
+}
 
 
 function setHover(){
@@ -76,13 +141,13 @@ function setHover(){
         var t;
         
         switch(id){
-            case "top":
+            case "top_button":
                 t = "トップ";
                 break;
-            case "prof":
+            case "prof_button":
                 t = "自己紹介";
                 break;
-            case "prod":
+            case "prod_button":
                 t = "制作物";
                 break;
         }
@@ -96,13 +161,13 @@ function setHover(){
         var bt;
         
         switch(id){
-            case "top":
+            case "top_button":
                 bt = "TOP";
                 break;
-            case "prof":
+            case "prof_button":
                 bt = "Profile";
                 break;
-            case "prod":
+            case "prod_button":
                 bt = "Product";
                 break;
         }
